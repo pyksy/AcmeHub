93dfd7ea5c9d4179a93b02bd454d4f3bf0866027
Antti Kultanen <antti.kultanen@molukki.com>
2025-04-10 18:38:43 +0300

    Generate sane data

--- generate_test_data.sh
+++ generate_test_data.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 URL="http://localhost:4080/process_report"
-SERVERS="8"
+SERVERS="16"
 STARTDATE="2025-01-01"
 DAYS="30"
-MAXSECONDS="600"
+DURATION="600"
+AVGVARIANCE="10"
+OUTLIERSVARIANCE="600"
+OUTLIERSRATIO="64"
 
 # server name array
 declare -a SERVERNAMES
@@ -20,13 +23,14 @@ for ((DAY=0; DAY<${DAYS}; DAY++))
 do
     for SERVERNAME in "${SERVERNAMES[@]}"
     do
-        DURATION="$((RANDOM % MAXSECONDS))"
+        DURATION="600"
+        [ $((RANDOM % OUTLIERSRATIO)) -eq 0 ] && DURATION="$((RANDOM % OUTLIERSVARIANCE + DURATION))" || DURATION="$((RANDOM % AVGVARIANCE + DURATION))"
         STARTSECONDS="${RANDOM}"
         STARTTIME="$(date -u -d "${STARTDATE} + ${DAY} days ${STARTSECONDS} seconds" +"%Y-%m-%dT%H:%M:%SZ")"
         ENDTIME="$(date -u -d "${STARTDATE} + ${DAY} days $((STARTSECONDS+DURATION)) seconds" +"%Y-%m-%dT%H:%M:%SZ")"
         POSTDATA="{\"server_name\":\"${SERVERNAME}\",\"start_time\":\"${STARTTIME}\",\"end_time\":\"${ENDTIME}\"}"
 
-        curl -s -d "${POSTDATA}" "${URL}" && echo -n "." || echo -n '!'
+        curl -s -o /dev/null -d "${POSTDATA}" "${URL}" && echo -n "." || echo -n '!'
     done
 done
 echo
